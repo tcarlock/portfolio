0dfc9b373153336bc3a2b55796c7ee0972c920a0
Tim Carlock <timothy.carlock@gmail.com>
2014-04-23 20:10:15 -0700

    Content and CSS update, upgrading bootstrap

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,17 @@ class Project < ActiveRecord::Base
   acts_as_taggable_on :tech, :skills
 
   has_many :attachments
+
+  has_attached_file :icon,
+    path: 'app/assets/images/project_images/:style/:id/:filename',
+    url: '/assets/project_images/:style/:id/:filename',
+    styles: {
+      large: ['1000x1540>', :png],
+      small: ['385x250>', :png],
+      thumb: ['200x130>', :png]
+    },
+    convert_options: {
+      :small => '-background transparent -strip',
+      :thumb => '-background transparent -strip'
+    }
 end
